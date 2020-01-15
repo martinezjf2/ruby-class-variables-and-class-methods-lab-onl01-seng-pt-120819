3a673da7ecf6436813523d67c2d0619156e9bb9a
Jeffrey Martinez <martinezjf2@gmail.com>
2020-01-15 19:38:57 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -34,7 +34,7 @@ class Song
   end
 
   def self.genre_count
-    binding.pry
+    # binding.pry
     @@genre_count = {}
     @@genres.each do |genre|
       if @@genre_count[genre]
@@ -44,6 +44,7 @@ class Song
       end
   end
   end
+
 
 
 end
